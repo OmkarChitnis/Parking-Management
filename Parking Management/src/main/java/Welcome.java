65d6f22a41092068444ed85217e037198b682287
steelium0811 <nimishgodbole409@gmail.com>
2022-11-01 21:05:35 +0530

    Welcome java made a few changes

--- Parking Management/src/main/java/Welcome.java
+++ Parking Management/src/main/java/Welcome.java
@@ -20,6 +20,7 @@ public class Welcome extends HttpServlet{
         String user = request.getParameter("username");
         String pass = request.getParameter("password");
         PrintWriter out = response.getWriter();
+        out.println("<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC\" crossorigin=\"anonymous\">");
         HttpSession sess = request.getSession();
     	sess.setAttribute("username", user);
     	sess.setAttribute("password", pass);
@@ -27,7 +28,16 @@ public class Welcome extends HttpServlet{
     	Cookie cpass = new Cookie("pass",pass);
     	response.addCookie(c);
     	response.addCookie(cpass);
-        out.println("Welcome");
+        out.println("<div class=\"toast\" role=\"alert\" aria-live=\"assertive\" aria-atomic=\"true\">\r\n"
+        		+ "  <div class=\"toast-header\">\r\n"
+        		+ "    <strong class=\"me-auto\">Welcome</strong>\r\n"
+        		+ "    <small>now</small>\r\n"
+        		+ "    <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"toast\" aria-label=\"Close\"></button>\r\n"
+        		+ "  </div>\r\n"
+        		+ "  <div class=\"toast-body\">\r\n"
+        		+ "    Hello USer\r\n"
+        		+ "  </div>\r\n"
+        		+ "</div>");
       }
 	protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
 		doPost(request,response);
