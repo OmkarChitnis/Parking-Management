d2d50c3d71991e2a47d9d70bdec06b233d0cf552
steelium0811 <nimishgodbole409@gmail.com>
2022-11-01 20:15:33 +0530

    Validate File

--- Parking Management/src/main/java/U_Validate.java
+++ Parking Management/src/main/java/U_Validate.java
@@ -6,25 +6,24 @@ import java.sql.ResultSet;
 public class U_Validate {
 	public static boolean checkUser(String user,String pass) 
     {
-        boolean st =false;
+        boolean st = false;
         try {
 
             //loading drivers for mysql
             Class.forName("com.mysql.jdbc.Driver");
 
             //creating connection with the database
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/parkingmanagement","root","mysql");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/parkingmanagement","root","Stormbreaker0811");
             PreparedStatement ps = con.prepareStatement("select * from user where u_username=? and u_password=?");
             ps.setString(1, user);
             ps.setString(2, pass);
             ResultSet rs =ps.executeQuery();
             st = rs.next();
-
         }
         catch(Exception e) {
             e.printStackTrace();
         }
-        return st;                 
+        return st;
     }
 
 }
