5b9a1551a7acf100479b72f40355bcfd552f6150
omkarchitnis1a <omkarchitnis1a@gmail.com>
2022-11-23 10:40:04 +0530

    conn

--- Parking Management/src/main/java/ConnectionDB.java
+++ Parking Management/src/main/java/ConnectionDB.java
@@ -12,20 +12,20 @@ public static Connection conn = null;
 
 public static Connection connect()
 {
-//	Properties props = new Properties();
-//	try
-//    {
-//        Class.forName("com.mysql.jdbc.Driver");  
-//        String url="jdbc:mysql://upszsyyyksaxwnso:zvT431LkkJUz3qEf452q@bhhdo6ctnwedt2vk8miw-mysql.services.clever-cloud.com:3306/bhhdo6ctnwedt2vk8miw";
-//        conn = DriverManager.getConnection(url,username,password);
-//        
-//    }
+	Properties props = new Properties();
+	try
+    {
+        Class.forName("com.mysql.jdbc.Driver");  
+        String url="jdbc:mysql://upszsyyyksaxwnso:zvT431LkkJUz3qEf452q@bhhdo6ctnwedt2vk8miw-mysql.services.clever-cloud.com:3306/bhhdo6ctnwedt2vk8miw";
+        conn = DriverManager.getConnection(url,username,password);
+        
+    }
 	
-	 ResourceBundle reader = null;
-	 try{ 
-	     reader = ResourceBundle.getBundle("dbconfig.properties");
-	     Connection conn=DriverManager.getConnection(reader.getString("db.url"),reader.getString("db.username"),reader.getString("db.password"));
-	 }
+//	 ResourceBundle reader = null;
+//	 try{ 
+//	     reader = ResourceBundle.getBundle("dbconfig.properties");
+//	     Connection conn=DriverManager.getConnection(reader.getString("db.url"),reader.getString("db.username"),reader.getString("db.password"));
+//	 }
 
 
     catch(Exception e)
