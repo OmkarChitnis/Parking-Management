3f5081ea20edc71dd82ca653ca3400cbe939ca3e
omkarchitnis1a <omkarchitnis1a@gmail.com>
2022-11-29 15:57:46 +0530

    a_login commited

--- Parking Management/src/main/java/A_Login.java
+++ Parking Management/src/main/java/A_Login.java
@@ -63,6 +63,7 @@ public class A_Login extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		//this is a comment
 	}
 
 }
